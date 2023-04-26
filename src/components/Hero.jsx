90b70b9939c2aef92cb0b7ec5e4bdaabdd88b400
unknown <you@example.com>
2023-04-27 04:44:40 +1300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+export default function Hero() {
+  return (
+    <div className='text-white'>
+
+<div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#00df9a] font-bold p-2 uppercase'>
+          Growing with data analytics
+        </p>
+        <h1 className='text-7xl max-md:text-5xl font-bold md:py-6'>
+          Grow with data.
+        </h1>
+        <div className='flex justify-center items-center'>
+          <p className='text-5xl max-md:text-4xl max-sm:text-2x  font-bold py-4'>
+            Fast, flexible financing for
+          </p>
+          
+        </div>
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+        <button className='bg-[#00df9a] w-[250px] rounded-md font-medium my-6 mx-auto py-5 text-black'>Get Started</button>
+      </div>
+
+
+    </div>
+  )
+}
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import { FaBars, FaTimes } from "react-icons/fa";
+
+export default function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="flex justify-between w-[80%] mx-auto items-center p-4">
+      <h1 className="text-3xl font-bold text-[#00df9a] uppercase max-md:hidden">
+        React
+      </h1>
+      <ul className="flex max-md:hidden">
+        <li className="text-white p-5">Home</li>
+        <li className="text-white p-5">Company</li>
+        <li className="text-white p-5">Contact</li>
+        <li className="text-white p-5">About</li>
+        <li className=" text-white p-5">Resource</li>
+      </ul>
+
+      <h1 className="text-3xl text-[#00df9a] font-bold uppercase md:hidden z-50 p-4">
+        React
+      </h1>
+
+      <ul
+        className={
+          nav
+            ? " w-[50%] md:hidden fixed top-[10%] left-0 ease-in-out duration-1000 z-50 "
+            : " w-full ease-in-out duration-1000 absolute top-[10%] left-[-100%] z-50 "
+        }
+      >
+        <li className=" bg-black text-white p-8 border-b border-gray-600">Home</li>
+        <li className="bg-black text-white p-8 border-b border-gray-600">Company</li>
+        <li className="bg-black text-white p-8 border-b border-gray-600">Resources</li>
+        <li className="bg-black text-white p-8 border-b border-gray-600">About</li>
+        <li className="bg-black text-white p-8">Contact</li>
+      </ul>
+
+      <div onClick={handleNav} className="text-[#00df9a] md:hidden z-50">
+        {nav ? <FaTimes /> : <FaBars />}
+      </div>
+    </div>
+  );
+}
